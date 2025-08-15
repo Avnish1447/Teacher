58668c84e418dcae1eb876e84faaafb1171f51e0
Avnish <160031022+Avnish1447@users.noreply.github.com>
2025-08-15 19:05:47 +0530

    Delete August directory

--- August/06.08/Problem - 1/solution.py
+++
@@ -1,9 +0,0 @@
-def Drink():
-    OrangeContainingDrinks = int(input(""))
-    OrangeJuiceVolumes = list(map(int, input().split()))
-    SumOfOrangeJuiceVolume = 0
-    for i in OrangeJuiceVolumes:
-        SumOfOrangeJuiceVolume +=  i
-    print(SumOfOrangeJuiceVolume/OrangeContainingDrinks)
-        
-Drink()    
--- August/06.08/Problem - 2/solution.py
+++
@@ -1,8 +0,0 @@
-def football():
-    situation = input("")
-    if '0000000' in situation or '1111111' in situation:
-        print("YES")
-    else:
-        print("NO")
-    
-football()
